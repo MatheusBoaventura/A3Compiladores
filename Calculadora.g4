38a9c6b738bdbdd77632c3106502e3e043fb0e2b
AgathaGoncalves <71910283+AgathaGoncalves@users.noreply.github.com>
2023-11-23 19:31:08 -0300

    Calculadora funcionando

---
+++ Calculadora.g4
@@ -0,0 +1,10 @@
+grammar Calculadora;
+ 
+prog: expr;
+expr: expr op=('*'|'/') expr  # OpBin
+    | expr op=('+'|'-') expr  # OpBin
+    | '(' expr ')'            # par
+    | INT                     # num
+    ;
+ 
+INT : ('0'..'9')+ ;
--- CalculadoraLexer.java
+++ CalculadoraLexer.java
@@ -1,4 +1,4 @@
-// Generated from Hello.g4 by ANTLR 4.13.1
+// Generated from Calculadora.g4 by ANTLR 4.13.1
 import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.Token;
@@ -9,14 +9,14 @@ import org.antlr.v4.runtime.dfa.DFA;
 import org.antlr.v4.runtime.misc.*;
 
 @SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast", "CheckReturnValue", "this-escape"})
-public class HelloLexer extends Lexer {
+public class CalculadoraLexer extends Lexer {
 	static { RuntimeMetaData.checkVersion("4.13.1", RuntimeMetaData.VERSION); }
 
 	protected static final DFA[] _decisionToDFA;
 	protected static final PredictionContextCache _sharedContextCache =
 		new PredictionContextCache();
 	public static final int
-		T__0=1, ID=2, WS=3;
+		T__0=1, T__1=2, T__2=3, T__3=4, T__4=5, T__5=6, INT=7;
 	public static String[] channelNames = {
 		"DEFAULT_TOKEN_CHANNEL", "HIDDEN"
 	};
@@ -27,20 +27,20 @@ public class HelloLexer extends Lexer {
 
 	private static String[] makeRuleNames() {
 		return new String[] {
-			"T__0", "ID", "WS"
+			"T__0", "T__1", "T__2", "T__3", "T__4", "T__5", "INT"
 		};
 	}
 	public static final String[] ruleNames = makeRuleNames();
 
 	private static String[] makeLiteralNames() {
 		return new String[] {
-			null, "'hello'"
+			null, "'*'", "'/'", "'+'", "'-'", "'('", "')'"
 		};
 	}
 	private static final String[] _LITERAL_NAMES = makeLiteralNames();
 	private static String[] makeSymbolicNames() {
 		return new String[] {
-			null, null, "ID", "WS"
+			null, null, null, null, null, null, null, "INT"
 		};
 	}
 	private static final String[] _SYMBOLIC_NAMES = makeSymbolicNames();
@@ -78,13 +78,13 @@ public class HelloLexer extends Lexer {
 	}
 
 
-	public HelloLexer(CharStream input) {
+	public CalculadoraLexer(CharStream input) {
 		super(input);
 		_interp = new LexerATNSimulator(this,_ATN,_decisionToDFA,_sharedContextCache);
 	}
 
 	@Override
-	public String getGrammarFileName() { return "Hello.g4"; }
+	public String getGrammarFileName() { return "Calculadora.g4"; }
 
 	@Override
 	public String[] getRuleNames() { return ruleNames; }
@@ -102,26 +102,29 @@ public class HelloLexer extends Lexer {
 	public ATN getATN() { return _ATN; }
 
 	public static final String _serializedATN =
-		"\u0004\u0000\u0003\u0019\u0006\uffff\uffff\u0002\u0000\u0007\u0000\u0002"+
-		"\u0001\u0007\u0001\u0002\u0002\u0007\u0002\u0001\u0000\u0001\u0000\u0001"+
-		"\u0000\u0001\u0000\u0001\u0000\u0001\u0000\u0001\u0001\u0004\u0001\u000f"+
-		"\b\u0001\u000b\u0001\f\u0001\u0010\u0001\u0002\u0004\u0002\u0014\b\u0002"+
-		"\u000b\u0002\f\u0002\u0015\u0001\u0002\u0001\u0002\u0000\u0000\u0003\u0001"+
-		"\u0001\u0003\u0002\u0005\u0003\u0001\u0000\u0002\u0001\u0000az\u0003\u0000"+
-		"\t\n\r\r  \u001a\u0000\u0001\u0001\u0000\u0000\u0000\u0000\u0003\u0001"+
-		"\u0000\u0000\u0000\u0000\u0005\u0001\u0000\u0000\u0000\u0001\u0007\u0001"+
-		"\u0000\u0000\u0000\u0003\u000e\u0001\u0000\u0000\u0000\u0005\u0013\u0001"+
-		"\u0000\u0000\u0000\u0007\b\u0005h\u0000\u0000\b\t\u0005e\u0000\u0000\t"+
-		"\n\u0005l\u0000\u0000\n\u000b\u0005l\u0000\u0000\u000b\f\u0005o\u0000"+
-		"\u0000\f\u0002\u0001\u0000\u0000\u0000\r\u000f\u0007\u0000\u0000\u0000"+
-		"\u000e\r\u0001\u0000\u0000\u0000\u000f\u0010\u0001\u0000\u0000\u0000\u0010"+
-		"\u000e\u0001\u0000\u0000\u0000\u0010\u0011\u0001\u0000\u0000\u0000\u0011"+
-		"\u0004\u0001\u0000\u0000\u0000\u0012\u0014\u0007\u0001\u0000\u0000\u0013"+
-		"\u0012\u0001\u0000\u0000\u0000\u0014\u0015\u0001\u0000\u0000\u0000\u0015"+
-		"\u0013\u0001\u0000\u0000\u0000\u0015\u0016\u0001\u0000\u0000\u0000\u0016"+
-		"\u0017\u0001\u0000\u0000\u0000\u0017\u0018\u0006\u0002\u0000\u0000\u0018"+
-		"\u0006\u0001\u0000\u0000\u0000\u0003\u0000\u0010\u0015\u0001\u0006\u0000"+
-		"\u0000";
+		"\u0004\u0000\u0007 \u0006\uffff\uffff\u0002\u0000\u0007\u0000\u0002\u0001"+
+		"\u0007\u0001\u0002\u0002\u0007\u0002\u0002\u0003\u0007\u0003\u0002\u0004"+
+		"\u0007\u0004\u0002\u0005\u0007\u0005\u0002\u0006\u0007\u0006\u0001\u0000"+
+		"\u0001\u0000\u0001\u0001\u0001\u0001\u0001\u0002\u0001\u0002\u0001\u0003"+
+		"\u0001\u0003\u0001\u0004\u0001\u0004\u0001\u0005\u0001\u0005\u0001\u0006"+
+		"\u0004\u0006\u001d\b\u0006\u000b\u0006\f\u0006\u001e\u0000\u0000\u0007"+
+		"\u0001\u0001\u0003\u0002\u0005\u0003\u0007\u0004\t\u0005\u000b\u0006\r"+
+		"\u0007\u0001\u0000\u0000 \u0000\u0001\u0001\u0000\u0000\u0000\u0000\u0003"+
+		"\u0001\u0000\u0000\u0000\u0000\u0005\u0001\u0000\u0000\u0000\u0000\u0007"+
+		"\u0001\u0000\u0000\u0000\u0000\t\u0001\u0000\u0000\u0000\u0000\u000b\u0001"+
+		"\u0000\u0000\u0000\u0000\r\u0001\u0000\u0000\u0000\u0001\u000f\u0001\u0000"+
+		"\u0000\u0000\u0003\u0011\u0001\u0000\u0000\u0000\u0005\u0013\u0001\u0000"+
+		"\u0000\u0000\u0007\u0015\u0001\u0000\u0000\u0000\t\u0017\u0001\u0000\u0000"+
+		"\u0000\u000b\u0019\u0001\u0000\u0000\u0000\r\u001c\u0001\u0000\u0000\u0000"+
+		"\u000f\u0010\u0005*\u0000\u0000\u0010\u0002\u0001\u0000\u0000\u0000\u0011"+
+		"\u0012\u0005/\u0000\u0000\u0012\u0004\u0001\u0000\u0000\u0000\u0013\u0014"+
+		"\u0005+\u0000\u0000\u0014\u0006\u0001\u0000\u0000\u0000\u0015\u0016\u0005"+
+		"-\u0000\u0000\u0016\b\u0001\u0000\u0000\u0000\u0017\u0018\u0005(\u0000"+
+		"\u0000\u0018\n\u0001\u0000\u0000\u0000\u0019\u001a\u0005)\u0000\u0000"+
+		"\u001a\f\u0001\u0000\u0000\u0000\u001b\u001d\u000209\u0000\u001c\u001b"+
+		"\u0001\u0000\u0000\u0000\u001d\u001e\u0001\u0000\u0000\u0000\u001e\u001c"+
+		"\u0001\u0000\u0000\u0000\u001e\u001f\u0001\u0000\u0000\u0000\u001f\u000e"+
+		"\u0001\u0000\u0000\u0000\u0002\u0000\u001e\u0000";
 	public static final ATN _ATN =
 		new ATNDeserializer().deserialize(_serializedATN.toCharArray());
 	static {
